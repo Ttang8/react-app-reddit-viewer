1dc06dd7eb48922d118790268056fa9b4f748b4e
Terrence Tang <thtang28@gmail.com>
2018-12-29 01:19:56 -0800

    Updates

---
+++ precache-manifest.73a9af38a83229f3827b8e5c59db638c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "30041714f98894a85474",
+    "url": "/react-app-reddit-viewer/static/js/runtime~main.30041714.js"
+  },
+  {
+    "revision": "db95ff37de8fc9b65da7",
+    "url": "/react-app-reddit-viewer/static/js/main.db95ff37.chunk.js"
+  },
+  {
+    "revision": "a19300d9d91194cdc66f",
+    "url": "/react-app-reddit-viewer/static/js/1.a19300d9.chunk.js"
+  },
+  {
+    "revision": "db95ff37de8fc9b65da7",
+    "url": "/react-app-reddit-viewer/static/css/main.e3b2e5b5.chunk.css"
+  },
+  {
+    "revision": "b399ceecd88f9f211093b4ae6a684b47",
+    "url": "/react-app-reddit-viewer/index.html"
+  }
+];
